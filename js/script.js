32866944cc64c725879217611547f41394ee136e
miekru <sajna.michal@gmail.com>
2022-09-22 13:31:27 +0200

    Add script.js player-choice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// ---------------------------COMPUTER CHOICE
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
@@ -7,7 +8,21 @@ if (randomNumber == '1') {
   computerMove = ' papier';
 } else if (randomNumber == '3') {
   computerMove = ' nożyce';
-} else {
-  computerMove = 'nieznany ruch';
-}
+} 
 printMessage('Mój ruch: ' + computerMove);
+
+// ---------------------------PLAYER CHOICE
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage('Błędny wybór');
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
